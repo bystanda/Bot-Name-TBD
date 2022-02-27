88b15a1efdbaec3b9103e3e29bf847812678a684
InterConnect <81645649+Inter-Connect@users.noreply.github.com>
2022-02-26 17:41:19 -0700

    Second for testing

--- index.js
+++ index.js
@@ -1,17 +1,1 @@
-console.log("Hey Lumins!")
-//Hey Lumins!!
-//I see your cursor ;)
-// oh hi 
-//Hola!
-
-// i prefer replit over this why is this so bad
-//yeah
-//replit has node.js 12.0
-//I have nodejs 16.0, which means we can make a bot with slash commands and all the new features. 
-//I've had bad experiences with those. Like, you have to run them every time you restart the program? like what.
-//Anywayy
-//I can get a template bot up and running real quick k?
-//alr
-// js comments go brrrr
-// you got the repo btw?
-// Gimme a sec
+// ayo
